15743b343ad41c19ca69b41ea697ed6bebe0d461
bn519 <137638934+bn519@users.noreply.github.com>
2023-12-05 20:37:32 -0500

    Add files via upload

---
+++ exchange.js
@@ -0,0 +1,33 @@
+document.getElementById("click").onclick = function() {submitForm(event)};
+
+function submitForm(e) {
+    e.preventDefault();
+    var output = document.getElementById("myform");
+    var display = document.getElementById("display");
+    var inputVals = output.querySelectorAll("input:not([type=submit]), select");
+
+    var amount = inputVals[0].value;
+    const currency = inputVals[1].value;
+
+    const apiUrl = "https://api.api-ninjas.com/v1/exchangerate?pair=USD_".concat(currency);
+   
+    fetch(apiUrl, {
+        method: 'GET',
+        headers: { 'X-Api-Key': apiKey},
+        contentType: 'application/json'
+    })
+    .then(response => response.json())
+    .then(data => {
+        //alert(JSON.stringify(data));
+        //alert(data.exchange_rate);
+        var rate = data.exchange_rate;
+        //alert(rate);
+        let resultPrice = amount * rate;
+        document.getElementById("display").innerHTML = `${amount} USD = ${resultPrice.toFixed(2)} ${currency}`;
+    })
+
+    .catch(error => {
+        alert('Request failed: ', error);
+        result.innerHTML = 'An error occured. Please try again later.';
+    })
+}
